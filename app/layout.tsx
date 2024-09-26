1669d9e2a558390f6e7f71de23d8330e99989f4f
Bilhakki <bilhakki@gmail.com>
2024-09-26 11:11:47 +0700

    tambah pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,17 +4,33 @@ import "./globals.css";
 
 // const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Peunajoh",
-  description: "",
-};
-
 import { Inter as FontSans } from "next/font/google";
 import Head from "next/head";
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
+
+export const metadata: Metadata = {
+  title: "Peunajoh",
+  description: "",
+  generator: "Peunajoh",
+  manifest: "/manifest.json",
+  keywords: ["peunajoh"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [
+    {
+      name: "STIKES Medika Seuramoe Barat",
+      url: "https://stikesmsb.ac.id/index.html",
+    },
+  ],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/ios/128.png" },
+    { rel: "icon", url: "icons/ios/128.png" },
+  ],
+};
 
 export default function RootLayout({
   children,
--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import withPWA from "next-pwa";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // reactStrictMode: true,
@@ -33,6 +35,19 @@ const nextConfig = {
   //     },
   //   ];
   // },
+
+  reactStrictMode: true, // Enable React strict mode for improved error handling
+  swcMinify: true, // Enable SWC minification for improved performance
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+  },
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public", // destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // disable PWA in the development environment
+  register: true, // register the PWA service worker
+  skipWaiting: true, // skip waiting for service worker activation
+})(nextConfig);
+
+// export default nextConfig;
